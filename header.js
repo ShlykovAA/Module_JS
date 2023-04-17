38e1c6c645454025da57e1e0eb7e16488f4edde5
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-04-17 15:41:15 +0300

    comments

--- header.js
+++ header.js
@@ -1,9 +1,13 @@
 const aboutMe = async () => {
-    const res = await fetch('https://api.github.com/users/ShlykovAA');
-    const data = await res.json();
-    const myPhoto = document.getElementById('about_me_img');
-    const myName = document.getElementById('about_me_name');
-    myPhoto.src = data.avatar_url;
-    myName.innerText = data.name;
-}
+  const res = await fetch("https://api.github.com/users/ShlykovAA");
+  const data = await res.json();
+  // I would split this code into to separate functions
+  // For example loadUserInfo and renderUserInfo
+  // Also func names should be verb or group of words that explains action
+
+  const myPhoto = document.getElementById("about_me_img");
+  const myName = document.getElementById("about_me_name");
+  myPhoto.src = data.avatar_url;
+  myName.innerText = data.name;
+};
 aboutMe();
--- todo.js
+++ todo.js
@@ -1,37 +1,45 @@
 function newElement() {
+  // add new element
+  // Comments in code is a bad practice
+  // Change func name and split this code into smaller functions
 
-    // add new element
+  const ul = document.getElementById("ul_list");
+  const li = document.createElement("li");
+  const inputText = document.getElementById("text_input").value;
+  if (inputText === "") {
+    alert("Input is empty!!!");
+  } else {
+    li.innerText = inputText;
+    ul.append(li);
+    document.getElementById("text_input").value = "";
+  }
 
-    const ul = document.getElementById('ul_list');
-    const li = document.createElement('li');
-    const inputText = document.getElementById('text_input').value;
-    if (inputText === '') {
-        alert('Input is empty!!!')
-    } else {
-        li.innerText = inputText;
-        ul.append(li);
-        document.getElementById('text_input').value = '';
-    }
+  // add close button
 
-    // add close button
+  const span = document.createElement("span");
+  span.innerText = "\u00D7";
+  span.className = "close";
+  li.append(span);
 
-    const span = document.createElement('span');
-    span.innerText = '\u00D7';
-    span.className = 'close';
-    li.append(span);
+  span.addEventListener("click", (event) => {
+    li.remove();
+  });
 
-    span.addEventListener('click', (event) => {
-        li.remove()
-    });
+  // add checked class
 
-    // add checked class
-
-    li.addEventListener('click', (event) => {
-        event.target.classList.toggle('checked')
-    });
+  li.addEventListener("click", (event) => {
+    event.target.classList.toggle("checked");
+  });
 }
 
-const addBtn = document.querySelector('.add_btn');
-addBtn.addEventListener('click', (event) => {
-    newElement();
+const addBtn = document.querySelector(".add_btn");
+addBtn.addEventListener("click", (event) => {
+  newElement();
 });
+// Don't create a new function if you don't need it
+// Also don't pass arguments if you don't use them
+// addBtn.addEventListener("click", newElement);
+
+// Consider using a class here
+// In constructor find element and add event listener
+// In methods do other logic

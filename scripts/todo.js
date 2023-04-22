8dda4d9c30cca0771408dc43587f07181561293d
Anatolii Shlykov <123013121+ShlykovAA@users.noreply.github.com>
2023-04-22 16:32:29 +0300

    code readability fix for todo

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,37 +1,36 @@
-function newElement() {
+const noteList = document.getElementById("ul_list");
+const addButton = document.querySelector(".add_btn");
 
-    // add new element
+function createNewElement() {
+  const newTegLi = document.createElement("li");
+  const newTegSpan = createCloseButton();
+  newTegLi.innerText = currentTextInInput();
+  newTegLi.append(newTegSpan);
+  noteList.append(newTegLi);
+  newTegSpan.addEventListener("click", () => {
+    newTegLi.remove();
+  });
+  newTegLi.addEventListener("click", (event) => {
+    event.target.classList.toggle("checked");
+  });
+  document.getElementById("text_input").value = "";
+};
 
-    const ul = document.getElementById('ul_list');
-    const li = document.createElement('li');
-    const inputText = document.getElementById('text_input').value;
-    if (inputText === '') {
-        alert('Input is empty!!!')
-    } else {
-        li.innerText = inputText;
-        ul.append(li);
-        document.getElementById('text_input').value = '';
-    }
+function createCloseButton(){
+  const newSpan = document.createElement("span");
+  newSpan.innerText = "\u00D7";
+  newSpan.className = "close";
+  return newSpan;
+};
 
-    // add close button
+function currentTextInInput() {
+  return document.getElementById("text_input").value;
+};
 
-    const span = document.createElement('span');
-    span.innerText = '\u00D7';
-    span.className = 'close';
-    li.append(span);
-
-    span.addEventListener('click', (event) => {
-        li.remove()
-    });
-
-    // add checked class
-
-    li.addEventListener('click', (event) => {
-        event.target.classList.toggle('checked')
-    });
-}
-
-const addBtn = document.querySelector('.add_btn');
-addBtn.addEventListener('click', (event) => {
-    newElement();
+addButton.addEventListener("click", () => {
+  if (currentTextInInput() === "") {
+    alert("Input is empty!!!");
+  } else {
+    createNewElement();
+  }
 });

2d5a22249e044573c5d20ce895e58c3f07425814
Anatolii Shlykov <123013121+ShlykovAA@users.noreply.github.com>
2023-04-22 15:12:06 +0300

    code readability fix for game

--- scripts/game.js
+++ scripts/game.js
@@ -1,71 +1,104 @@
-// game field
-
-const cells = document.querySelectorAll('#field td');
-
-// game
+const cells = document.querySelectorAll("#field td");
+const gameScoreTable = document.querySelector(".div_for_game_score");
+const newGameBtn = document.getElementById("new_game");
+let counter = 0;
 
 function startGame(cells) {
-    let counter = 0;
-    const isVictory = (cells) => {
-        if (cells[0].innerText === cells[1].innerText && cells[1].innerText === cells[2].innerText && cells[0].innerText !== '') {
-            return true;
-        } else if (cells[3].innerText === cells[4].innerText && cells[4].innerText === cells[5].innerText && cells[3].innerText !== '') {
-            return true;
-        } else if (cells[6].innerText === cells[7].innerText && cells[7].innerText === cells[8].innerText && cells[6].innerText !== '') {
-            return true;
-        } else if (cells[0].innerText === cells[3].innerText && cells[3].innerText === cells[6].innerText && cells[0].innerText !== '') {
-            return true;
-        } else if (cells[1].innerText === cells[4].innerText && cells[4].innerText === cells[7].innerText && cells[1].innerText !== '') {
-            return true;
-        } else if (cells[2].innerText === cells[5].innerText && cells[5].innerText === cells[8].innerText && cells[2].innerText !== '') {
-            return true;
-        } else if (cells[0].innerText === cells[4].innerText && cells[4].innerText === cells[8].innerText && cells[0].innerText !== '') {
-            return true;
-        } else if (cells[2].innerText === cells[4].innerText && cells[4].innerText === cells[6].innerText && cells[2].innerText !== '') {
-            return true;
-        } else {
-            return false;
-        }
-    };
-    const clickOn = (event) => {
-        const elem = document.querySelector('.div_for_game_score')
-        const newTeg = document.createElement('h3');
-        if (counter%2 === 0) {
-                event.target.innerText = 'X';
-        } else {
-                event.target.innerText = 'O';
-        }
-        event.target.removeEventListener('click', clickOn)
-        if (isVictory(cells)) {
-            newTeg.innerText = counter%2 === 0 ? 'Winner sing "X"' : 'Winner sing "O"';
-            elem.append(newTeg);
-            for (let cell of cells) {
-                cell.removeEventListener('click', clickOn);
-            };
-        } else if (counter === 8) {
-            newTeg.innerText = 'Draw, no one winner';
-            elem.append(newTeg);
-            for (let cell of cells) {
-                cell.removeEventListener('click', clickOn);
-            };
-        }
-        counter++;
-    }
-    for (let cell of cells) {
-        cell.addEventListener('click', clickOn)
-    }
-    
+  counter = 0;
+  for (let cell of cells) {
+    cell.addEventListener("click", clickOn);
+  }
 };
 
-startGame(cells);
+function clearCells() {
+  for (let cell of cells) {
+    cell.removeEventListener("click", clickOn);
+  }
+};
 
-// new game
+function clickOn(event) {
+  if (counter % 2 === 0) {
+    event.target.innerText = "X";
+  } else {
+    event.target.innerText = "O";
+  }
+  event.target.removeEventListener("click", clickOn);
+  addWinnerToGameScore();
+  counter++;
+};
 
-const newGameBtn = document.getElementById('new_game');
+function addWinnerToGameScore() {
+  const createNewTeg = document.createElement("h3");
+  if (isVictory(cells)) {
+    createNewTeg.innerText = counter % 2 === 0 ? 'Winner sing "X"' : 'Winner sing "O"';
+    gameScoreTable.append(createNewTeg);
+    clearCells();
+  } else if (counter === 8) {
+    createNewTeg.innerText = "Draw, no one winner";
+    gameScoreTable.append(createNewTeg);
+    clearCells();
+  }
+};
 
-newGameBtn.addEventListener('click', (event) => {
-    for (let cell of cells) {
-        cell.innerText = '';
-    }
-    startGame(cells);
+function isVictory(cells) {
+  if (
+    cells[0].innerText === cells[1].innerText &&
+    cells[1].innerText === cells[2].innerText &&
+    cells[0].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[3].innerText === cells[4].innerText &&
+    cells[4].innerText === cells[5].innerText &&
+    cells[3].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[6].innerText === cells[7].innerText &&
+    cells[7].innerText === cells[8].innerText &&
+    cells[6].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[0].innerText === cells[3].innerText &&
+    cells[3].innerText === cells[6].innerText &&
+    cells[0].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[1].innerText === cells[4].innerText &&
+    cells[4].innerText === cells[7].innerText &&
+    cells[1].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[2].innerText === cells[5].innerText &&
+    cells[5].innerText === cells[8].innerText &&
+    cells[2].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[0].innerText === cells[4].innerText &&
+    cells[4].innerText === cells[8].innerText &&
+    cells[0].innerText !== ""
+  ) {
+    return true;
+  } else if (
+    cells[2].innerText === cells[4].innerText &&
+    cells[4].innerText === cells[6].innerText &&
+    cells[2].innerText !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+newGameBtn.addEventListener("click", (event) => {
+  for (let cell of cells) {
+    cell.innerText = "";
+  }
+  startGame(cells);
 });
+
+startGame(cells);
